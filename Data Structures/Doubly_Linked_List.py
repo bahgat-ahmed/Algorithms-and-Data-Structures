07570750fddaeb79f04a661045cf4fec5f8a2a20
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-02-14 07:18:40 +0200

    adding insert method

--- Data Structures/Doubly_Linked_List.py
+++ Data Structures/Doubly_Linked_List.py
@@ -7,7 +7,7 @@ class Node():
 
 class DoublyLinkedList():
     
-    # the constructor
+    # the initializer
     def __init__(self):
         # no data in the head
         self.head = None
@@ -46,7 +46,7 @@ class DoublyLinkedList():
             self.tail.next = new_node
             # let the data in the LinkedList tail be the new node data thus updating the tail value
             self.tail = new_node
-        # increment length by 1
+        # incrementing Linked List length by 1
         self.length += 1
         
     def prepend(self, data):
@@ -62,5 +62,33 @@ class DoublyLinkedList():
             self.head.previous = new_node
             # let the data in the LinkedList head be the new node data thus updating the head value
             self.head = new_node
-        # increment length by 1    
+        # incrementing Linked List length by 1
         self.length += 1
+        
+    def insert(self, position, data):
+
+        if position == 0:
+            # inserting at the first position is the same as prepending
+            self.prepend(data)
+        elif position >= self.length:
+            # inserting at the last position is the same as appending
+            self.append(data)
+        else:
+            # creating a new node
+            new_node = Node(data)
+            # making a pointer to the Linked List head
+            current_node = self.head
+            # traversing until reaching the position just before the desired position
+            for i in range(position - 1):
+                current_node = current_node.next
+            # making the previous of the new_node point to the curernt node
+            new_node.previous = current_node
+            # making the next of the new_node point to the next of the current node
+            new_node.next = current_node.next
+            # making the next of the current node point to the new_node
+            current_node.next = new_node
+            # making the previous of the next node point to the new_node instead of the
+            # current_node
+            new_node.next.previous = new_node
+            # incrementing Linked List length by 1
+            self.length += 1
