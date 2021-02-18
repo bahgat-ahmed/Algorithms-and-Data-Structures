db18a4de89bbd07044b19d272c07d548d652c66a
Bahgat Ahmed <Bahgat-Ahmed67@hotmail.com>
2021-02-18 08:00:15 +0200

    Binary Search Tree with an insert method

---
+++ Data Structures/Binary_Search_Tree.py
@@ -0,0 +1,36 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+        
+class BST():
+    def __init__(self):
+        self.root = None
+        self.num_of_nodes = 0
+        
+    def insert(self, data):
+        new_node = Node(data)
+        if self.root == None:
+            self.root = new_node    
+        else:
+            current_node = self.root
+            while (current_node.right != new_node) and (current_node.left != new_node):
+                try:
+                    if (data > current_node.data):
+                        if current_node.right != None:
+                            current_node = current_node.right
+                        else:
+                            current_node.right = new_node
+
+                    elif (data < current_node.data):
+                        if current_node.left != None:
+                            current_node = current_node.left
+                        else:
+                            current_node.left = new_node
+                    else:
+                        raise ValueError("This value already exists in the Binary Search Tree.")
+                except ValueError:
+                    raise
+                    
+        self.num_of_nodes += 1
